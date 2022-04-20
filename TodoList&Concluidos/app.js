e7fef6d8b83a87365629826bf7b1be44141dab35
Pedro Alves <pedro.alves1705@gmail.com>
2022-04-19 22:30:59 -0300

    Add files via upload
    uploads has been done to avoid double checked task and working with arrays

--- TodoList&Concluidos/app.js
+++ TodoList&Concluidos/app.js
@@ -12,11 +12,14 @@ const erro = document.querySelector(".erro")
 const botaoCadastra = document.querySelector(".botao_adc")
 /* botão Limpar Lista */
 const botaoLimparLista = document.querySelector(".botao_del_todos")
+const botaoLimparListaConcluidos = document.querySelector(".botao_del_todos_concluidos")
 /* acessa as paletas */
-const paleta1= document.getElementById('paleta1'); 
+const paleta1= document.getElementById('paleta1');
 const paleta2= document.getElementById('paleta2');
 const paleta3= document.getElementById('paleta3');
 
+
+var contador=0;
 
 /* Arrays */
 const arrayToDoList = []
@@ -32,7 +35,7 @@ paleta3.addEventListener('click', () => definePaleta('tomato'))
 botaoCadastra.addEventListener('click', ()=> cadastraAtividade())
 //limpar lista
 botaoLimparLista.addEventListener('click',()=> removeAtividades())
-/* botao limpar linha individual */
+botaoLimparListaConcluidos.addEventListener('click', ()=> removeConcluidos())
 
 
 
@@ -45,30 +48,39 @@ function definePaleta(cor){
 /* Criar as Atividades na Lista */
 
 function criaAtividade(){
+    contador=contador+1;
+
     const atividade = document.createElement("div")
     atividade.classList.add('atividade')
-    const nomeAtividade = document.createElement('p')
-    atividade.textContent = input.value //define o texto do elemento - guardar o valor inserido pelo usuário
-    arrayToDoList.push(atividade.textContent)
-    
+   
+
+    const atividade_texto = document.createElement("div")
+    atividade_texto.classList.add('texto_atividade')
+    atividade_texto.setAttribute('id','atividade_'+contador)
+    atividade_texto.textContent = input.value //define o texto do elemento - guardar o valor inserido pelo usuário
+   
+   
     const botaoLimpar = document.createElement('button')
     botaoLimpar.textContent = 'limpar'
     botaoLimpar.classList.add('botao_del')
-    botaoLimpar.addEventListener('click', ()=> removeUmaAtividade(atividade))
+    botaoLimpar.addEventListener('click', ()=> removeUmaAtividade(atividade, atividade_texto))
 
     const botaoConcluir = document.createElement('button')
     botaoConcluir.textContent = 'OK'
     botaoConcluir.classList.add('botao_concluir')
     botaoConcluir.addEventListener('click', ()=> concluiAtividade(atividade))
 
-    
+   
     listaAtividades.appendChild(atividade)
-    atividade.appendChild(nomeAtividade)
+    atividade.appendChild(atividade_texto)
     atividade.appendChild(botaoLimpar)
     atividade.appendChild(botaoConcluir)
-    
+    arrayToDoList.push(atividade_texto.textContent)
+   
 
 }
+
+
 
 
 function cadastraAtividade(){
@@ -102,48 +114,65 @@ console.log(arrayConcluidos)
 /* Conclui atividades */
 
 function concluiAtividade(atividade){
+    contador += 1
     const itemConcluido = atividade.textContent.substring(0, atividade.textContent.length -8)  //remove o texto 'limparOk' no final
-    
-
+    if(arrayConcluidos.indexOf(itemConcluido) !== -1){
+        alert("Voce ja concluiu essa atividade")
+    }else{
     const concluido = document.createElement("div")
     concluido.classList.add('concluido')
-    concluido.textContent = itemConcluido
+    //concluido.setAttribute('id','concluido_'+contador)
+   // concluido.textContent = itemConcluido
+
+    const concluido_texto = document.createElement("div")
+    concluido_texto.classList.add('texto_concluido')
+    concluido_texto.setAttribute('id','concluido_'+contador)
+    concluido_texto.textContent = itemConcluido
 
     const botaoLimpar = document.createElement('button')
     botaoLimpar.textContent = 'limpar'
     botaoLimpar.classList.add('botao_del')
-    botaoLimpar.addEventListener('click', ()=> removeUmConcluido(concluido))
+    botaoLimpar.addEventListener('click', ()=> removeUmConcluido(concluido, concluido_texto))
 
 
     listaConcluidos.appendChild(concluido)
     concluido.appendChild(botaoLimpar)
-    arrayConcluidos.push(concluido.textContent.substring(0, concluido.textContent.length -6)) //remove o 'limpar' do final
-}
-
-
-
-/* Limpar as atividades e concluidos */
-
-function removeUmConcluido(concluido){
-    listaConcluidos.removeChild(concluido)
-}
-
-
-function removeUmaAtividade(atividade){
-    listaAtividades.removeChild(atividade)
-
-}
-
-function removeAtividades(){
-    while(listaAtividades.firstElementChild){ //enquanto houver elementos filhos
-        listaAtividades.removeChild(listaAtividades.firstElementChild)
+    concluido.appendChild(concluido_texto)
+    arrayConcluidos.push(concluido_texto.textContent)
     }
 }
 
 
+/* Limpar as atividades e concluidos */
 
+function removeUmConcluido(concluido, concluido_texto){
+    let apagarNoArray = concluido_texto.innerHTML
+    let indice = arrayConcluidos.indexOf(apagarNoArray)
+    arrayConcluidos.splice(indice, 1)
 
+    listaConcluidos.removeChild(concluido)
 
+}
 
+function removeUmaAtividade(atividade,atividade_texto ){
+    let apagarNoArray = atividade_texto.innerHTML
+    let indice = arrayToDoList.indexOf(apagarNoArray)
+    arrayToDoList.splice(indice, 1)
+    
+    listaAtividades.removeChild(atividade)
 
+}
 
+function removeConcluidos(){
+    while(listaConcluidos.firstElementChild){ //enquanto houver elementos filhos
+        arrayConcluidos.pop()
+        listaConcluidos.removeChild(listaConcluidos.firstElementChild)
+    }
+}
+
+function removeAtividades(){
+    while(listaAtividades.firstElementChild){ //enquanto houver elementos filhos
+        arrayToDoList.pop()
+        listaAtividades.removeChild(listaAtividades.firstElementChild)
+    }
+}
